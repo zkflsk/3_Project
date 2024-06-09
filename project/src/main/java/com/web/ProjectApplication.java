f15e956d969257258e6f6a2bf0db481ce8b8cb81
zkflsk <kwonkubi@gmail.com>
2024-06-09 14:46:27 +0900

    첫 커밋

---
+++ project/src/main/java/com/web/ProjectApplication.java
@@ -0,0 +1,34 @@
+package com.web;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	private static final Logger logger = 
+			LoggerFactory.getLogger(ProjectApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner testDatabaseConnection(DataSource dataSource) {
+		return args -> {
+			try {
+				dataSource.getConnection().close();
+				logger.info("데이터베이스 연결 성공");
+			}catch (Exception e) {
+				logger.error("데이터베이스 연결 실패", e);
+			}
+		};
+	}
+
+}
